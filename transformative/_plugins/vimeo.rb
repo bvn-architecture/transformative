6c4115119d1da55ef687d5468975cd5c5ee3a9d7
Ben <ben@notionparallax.co.uk>
2015-03-26 10:14:48 +1100

    make video embed patterns, close #19

---
+++ transformative/_plugins/vimeo.rb
@@ -0,0 +1,21 @@
+class Vimeo < Liquid::Tag
+  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+
+  def initialize(tagName, markup, tokens)
+    super
+
+    if markup =~ Syntax then
+      @id = $1
+    else
+      raise "No Vimeo ID provided in the \"vimeo\" tag"
+    end
+  end
+
+  def render(context)
+    "<div class=\"embed-responsive embed-responsive-4by3\">
+    <iframe class=\"embed-responsive-item\" src=\"https://player.vimeo.com/video/9468855\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    </div>"
+  end
+
+  Liquid::Template.register_tag "vimeo", self
+end
