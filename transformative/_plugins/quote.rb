c31dbdeb6d741ea7f964d3aac31c4fd72cbabf44
Ben <ben@notionparallax.co.uk>
2015-04-13 16:12:36 +1000

    Close #57

--- transformative/_plugins/quote.rb
+++ transformative/_plugins/quote.rb
@@ -3,19 +3,26 @@ class Quote < Liquid::Block
 
   def initialize(tagName, markup, tokens)
     super
-
-    @x=JSON.parse markup
+    puts "|" + markup + "|"
+    if markup.to_s == ''
+      markup = "{\"dummy\":\"\"}"#I don't like this at all!
+    end
+    @x = JSON.parse markup
   end
 
   def render(context)
-    " <figure class=\"quote\">
-        <blockquote>#{super}</blockquote>
-        <figcaption>
-         <span class=\"person\">#{@x["person"]}</span>,
-         <cite>#{@x["source"]}</cite>
-         <span class=\"date\">(#{@x["date"]})</span>
-        </figcaption>
-      </figure>"
+    person = "<span class=\"person\">#{@x["person"]}</span>" if @x["person"]
+    source = ", <cite>#{@x["source"]}</cite>"                if @x["source"]
+    date   = " <span class=\"date\">(#{@x["date"]})</span>"  if @x["date"]
+
+    quote_markup = "<figure class=\"quote\">"            +
+                   "  <blockquote>#{super}</blockquote>" +
+                   "  <figcaption>"                      +
+                   "    #{person}#{source}#{date}"       +
+                   "  </figcaption>"                     +
+                   "</figure>"
+
+    return quote_markup
   end
 
   Liquid::Template.register_tag "quote", self
